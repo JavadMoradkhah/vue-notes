8aa3e27aa8232e2d1c47535e84f6b6dfdf8e9c5f
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-12-13 16:17:15 +0330

    feat: Building the AddButton component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from './components/layout/Header.vue';
 import NotesList from './components/common/NotesList.vue';
+import AddButton from './components/common/AddButton.vue';
 </script>
 
 <template>
   <Header />
   <NotesList />
+  <AddButton />
 </template>
---
+++ src/components/common/AddButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AddIcon from '../icon/AddIcon.vue';
+</script>
+
+<template>
+  <a class="fixed bottom-6 right-6 p-2 bg-primary text-white rounded-full" href="/create-note">
+    <AddIcon width="36px" height="36px" />
+  </a>
+</template>
