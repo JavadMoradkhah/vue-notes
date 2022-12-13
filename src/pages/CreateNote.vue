413d2b7898b56c796873fd99dc38e775f61d8fd0
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-12-13 17:19:31 +0330

    feat: Building the CreateNote page

---
+++ src/pages/CreateNote.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <section class="max-w-2xl mx-auto px-4 py-8">
+      <h1 class="mb-4 text-primary text-2xl text-center font-bold">Create Note</h1>
+      <form @submit.prevent>
+        <fieldset>
+          <label class="block pb-1 text-gray-700" for="title-input">Title</label>
+          <input
+            id="title-input"
+            class="block w-full px-4 py-2 border rounded-md outline-none focus:border-primary"
+            name="title"
+            type="text"
+            placeholder="Enter the title..."
+          />
+        </fieldset>
+        <fieldset class="mt-3">
+          <label class="block pb-1 text-gray-700" for="body-input">Body</label>
+          <textarea
+            id="body-input"
+            class="block w-full min-h-[100px] px-4 py-2 border rounded-md outline-none focus:border-primary"
+            placeholder="Type post body..."
+            name="body"
+          ></textarea>
+        </fieldset>
+        <button
+          class="block w-full mt-4 px-4 py-2 text-white bg-primary rounded-md active:bg-emerald-600"
+          type="submit"
+        >
+          Create
+        </button>
+      </form>
+    </section>
+  </main>
+</template>
