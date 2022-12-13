b0b6d3e181cf5da68c4bcd09c8754bd87135da80
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-12-13 20:22:49 +0330

    feat: Building the Logo component

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,7 +1,11 @@
+<script setup>
+import Logo from './Logo.vue';
+</script>
+
 <template>
   <header class="fixed top-0 left-0 right-0 bg-white shadow-sm z-50">
     <div class="layout-container px-4 py-2">
-      <router-link class="text-primary text-2xl font-bold" to="/">Vue Notes</router-link>
+      <Logo />
     </div>
   </header>
 </template>
---
+++ src/components/layout/Logo.vue
@@ -0,0 +1,5 @@
+<template>
+  <router-link class="text-primary text-xl font-bold tracking-wide" to="/">
+    Vue<span class="text-secondary text-lg">Notes</span>
+  </router-link>
+</template>
