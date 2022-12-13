a38c8172298c9b1aa5075844594532b4b3547adb
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-12-13 18:40:22 +0330

    refactor: Note.vue

--- src/components/common/Note.vue
+++ src/components/common/Note.vue
@@ -8,6 +8,6 @@ const props = defineProps(['note']);
   <li class="p-4 bg-white rounded-md shadow-sm">
     <box-icon name="rocket"></box-icon>
     <h2 class="text-gray-800 text-lg font-bold line-clamp-1">{{ props.note.title }}</h2>
-    <p class="mt-2 text-gray-600 text-sm line-clamp-4">{{ props.note.body }}</p>
+    <p class="min-h-[100px] mt-2 text-gray-600 text-sm line-clamp-5">{{ props.note.body }}</p>
   </li>
 </template>
