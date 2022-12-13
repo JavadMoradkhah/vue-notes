5558b4c8575b7f74739530cc4f82b27d30355c86
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-12-13 18:17:39 +0330

    feat: Building the DataNotFound component

---
+++ src/components/layout/DataNotFound.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['text']);
+</script>
+
+<template>
+  <div class="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+    <p class="text-center">{{ props.text }}</p>
+  </div>
+</template>
