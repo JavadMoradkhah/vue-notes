416b226d9d84555dcdb3ef96fcd7fadbf71c16f7
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-12-13 20:08:36 +0330

    feat: Building the BackButton component

---
+++ src/components/common/BackButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineProps } from 'vue';
+import BackIcon from '../icon/BackIcon.vue';
+
+const props = defineProps(['url']);
+</script>
+
+<template>
+  <router-link class="flex items-center text-primary" :to="props.url">
+    <BackIcon width="1.6rem" height="1.6rem" />
+    <span class="font-bold text-lg">Back</span>
+  </router-link>
+</template>
