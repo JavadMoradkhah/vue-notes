30eedcf091089dc3e4c441e39cf5677f1f5791c0
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-12-13 17:19:55 +0330

    feat: Building the Main page

---
+++ src/pages/Main.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NotesList from '../components/common/NotesList.vue';
+import AddButton from '../components/common/AddButton.vue';
+</script>
+
+<template>
+  <main>
+    <NotesList />
+    <AddButton />
+  </main>
+</template>
